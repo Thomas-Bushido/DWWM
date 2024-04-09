9d424b22e84527e21d8bcd09a053f78c159a464d
Thomas-Bushido <thomas.pereira59@outlook.fr>
2024-04-09 13:55:23 +0200

    090424

--- HTMLCSS/JS/Jeu Cartes/js.js
+++ HTMLCSS/JS/Jeu Cartes/js.js
@@ -1,30 +1,60 @@
-let Startbutton = document.getElementById("Startbutton");
-let Readyfight = document.getElementById("ReadyFight");
 let Accueil = document.getElementById("Accueil");
-let SelectPlayer = document.getElementById("button1");
+let Startbutton = document.getElementById("Startbutton");
+
+let Pseudobutton = document.getElementById("button1");
+let MenuPseudo = document.getElementById("Choixpseudo")
+
+let Readyfight = document.getElementById("ReadyFight");
+let SelectPlayer = document.getElementById("button2");
 let MenuPlayer = document.getElementById("MenuPlayer");
 let Rockiselection = document.getElementById("PhotoRocki");
 let Papermanselection = document.getElementById("PhotoPaperman");
 let Scissorhandsselection = document.getElementById ("PhotoScissorhands");
+
+
+
+
 
 Startbutton.addEventListener("click", function() {
      Startbutton.style.display = "none";
      Readyfight.style.display = "block";
      Accueil.style.display = "none";
      MenuPlayer.style.display = "none";
-     
-     
 });
+
+Pseudobutton.addEventListener("click", function() {
+     MenuPseudo.style.display = "block";
+     Pseudobutton.style.display = "none";
+     SelectPlayer.style.display ="none";
+     MenuPlayer.style.display = "none";
+     Accueil.style.display = "none";
+     Readyfight.style.display = "none";
+ });
 
 SelectPlayer.addEventListener("click", function() {
     SelectPlayer.style.display ="none";
     Readyfight.style.display = "none";
     MenuPlayer.style.display = "flex";
     Accueil.style.display = "none";
+    MenuPseudo.style.display = "none";
 });
 
 
 Rockiselection.addEventListener("click", function() {
-    this.classList.add("button-clique1");
+   this.classList.add("button-clique1");
+    this.classList.remove("button-clique2");
+    this.classList.remove("button-clique3");
+});
+
+Papermanselection.addEventListener("click", function() {
+    this.classList.add("button-clique2");
+    this.classList.remove("button-clique1");
+    this.classList.remove("button-clique3");
+});
+
+Scissorhandsselection.addEventListener("click", function() {
+    this.classList.add("button-clique3");
+    this.classList.remove("button-clique2");
+    this.classList.remove("button-clique1");
 });
 
