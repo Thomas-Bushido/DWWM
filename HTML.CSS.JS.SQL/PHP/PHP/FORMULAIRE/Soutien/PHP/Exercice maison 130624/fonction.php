a1c2ffa44b4c1b3af20cab0c4b5c5ce0515e36cd
Thomas-Bushido <thomas.pereira59@outlook.fr>
2024-06-21 16:26:29 +0200

    210624

--- HTML.CSS.JS.SQL/PHP/PHP/FORMULAIRE/Soutien/PHP/Exercice maison 130624/fonction.php
+++ HTML.CSS.JS.SQL/PHP/PHP/FORMULAIRE/Soutien/PHP/Exercice maison 130624/fonction.php
@@ -1,78 +1,86 @@
 <?php
-$Livres =[];
+$Biblio =[];
 $Livre1 = ['ID' => 1, 'Titre' => 'Hello', 'Auteur' => 'Pierre', 'Edition' => "A"];
 $Livre2 = ['ID' => 2, 'Titre' => 'Goodbye','Auteur' => 'Paul', 'Edition' => "B"];
 $Livre3 = ['ID' => 3, 'Titre' => 'Hi','Auteur' => 'Jacques', 'Edition' => "C"];
 $Livre4 = ['ID' => 4, 'Titre' => 'Youhou','Auteur' => 'Max', 'Edition' => "D"];
 
 
-$Livres = [$Livre1, $Livre2, $Livre3, $Livre4];
+$biblio= [$Livre1, $Livre2, $Livre3, $Livre4];
 
-// function AfficherLivre(&$Livres, $ID, $Livre1, $Livre2, $Livre3, $Livre4){
+function AfficherLivre($biblio){
 
-//       switch($ID){
-//            case 1:
+       foreach($biblio as $id => $Livre){
+          echo "ID: " . $id;
+          echo "\n";
+          echo "Le titre est: " . $Livre['Titre'];
+          echo "\n";
+          echo "Le titre est: " . $Livre['Auteur'];
+          echo "\n";
+          echo "Le titre est: " . $Livre['Edition'];
+          echo "\n";
+          echo "\n";
+          echo "*************************************";
+          echo "\n";
+        
+       }
+    
+    //   switch($ID){
+    //        case 1:
             
-//            echo 'Titre: '. $Livre1['Titre']. "\n";
-//            echo 'Auteur: ' . $Livre1['Auteur']. "\n";
-//            echo 'Edition: ' . $Livre1['Edition']. "\n";
-//            break;
-//            case 2:
+    //        echo 'Titre: '. $Livre1['Titre']. "\n";
+    //        echo 'Auteur: ' . $Livre1['Auteur']. "\n";
+    //        echo 'Edition: ' . $Livre1['Edition']. "\n";
+    //        break;
+    //        case 2:
              
-//             echo 'Titre: '. $Livre2['Titre']. "\n";
-//             echo 'Auteur: ' . $Livre2['Auteur'];
-//             echo 'Edition: ' . $Livre2['Edition'];
-//             break;
-//             case 3:
+    //         echo 'Titre: '. $Livre2['Titre']. "\n";
+    //         echo 'Auteur: ' . $Livre2['Auteur'];
+    //         echo 'Edition: ' . $Livre2['Edition'];
+    //         break;
+    //         case 3:
                   
-//             echo 'Titre: '. $Livre3['Titre'];
-//             echo 'Auteur: ' . $Livre3['Auteur'];
-//             echo 'Edition: ' . $Livre3['Edition'];
-//             break;
-//             case 4:
-//             echo 'Titre: '. $Livre4['Titre'];
-//             echo 'Auteur: ' . $Livre4['Auteur'];
-//             echo 'Edition: ' . $Livre4['Edition'];
-//             break;
+    //         echo 'Titre: '. $Livre3['Titre'];
+    //         echo 'Auteur: ' . $Livre3['Auteur'];
+    //         echo 'Edition: ' . $Livre3['Edition'];
+    //         break;
+    //         case 4:
+    //         echo 'Titre: '. $Livre4['Titre'];
+    //         echo 'Auteur: ' . $Livre4['Auteur'];
+    //         echo 'Edition: ' . $Livre4['Edition'];
+    //         break;
 
-//       }
+    //   }
 
-// }
-
-
- function AfficherLivre(&$Livres, $ID, $Livre1, $Livre2, $Livre3, $Livre4);
+}
 
 
+//  function AfficherLivre(&$Livres, $ID, $Livre1, $Livre2, $Livre3, $Livre4);
 
-
-
-
-
-// function Ajouter(&$Bibliotheque, $Auteur, $Edition, $Titre, $Annee ){
-
-//       $nouveauLivre = [
-//                        'ID' => count($Bibliotheque) + 1,
-//                        'Auteur' => $Auteur,
-//                        'Titre' => $Titre,
-//                        'Année' => $Annee,
-//                         'Edition' => $Edition];
+function Ajouter(&$biblio, $Auteur, $Edition, $Titre, $Annee ){
+    $nouveauID = count($biblio) + 1;
+      $nouveauLivre = [
+                       'ID' => $nouveauID,
+                       'Auteur' => $Auteur,
+                       'Titre' => $Titre,
+                       'Année' => $Annee,
+                        'Edition' => $Edition];
   
-//                   return  $Bibliotheque[]= $nouveauLivre;
+                  return  $biblio[]= $nouveauLivre;
                
-// }
+}
 
 
-// function modifierLivre(&$Livres, $ModifAuteur, $ModifEdition, $ChoixID){
-      
-//      foreach($Livres as $Livre){
-//       if($Livre[0] == $ChoixID){
-//           $Livre['Auteur'] = $ModifAuteur;
-//           $Livre['Edition'] = $ModifEdition;
 
-//        return;
-          
-//       }
-// }
-// }
-
-
+function modifierLivre($biblio, $ChoixID, $ModifAuteur, $ModifEdition) {
+    foreach ($biblio as &$Livre) { // Utilisation de référence pour modifier directement l'élément du tableau
+        if ($Livre['ID'] == $ChoixID) {
+            $Livre['Auteur'] = $ModifAuteur;
+            echo $Livre['Auteur'];
+            $Livre['Edition'] = $ModifEdition;
+            echo $Livre['Edition'];
+            break; // Sortir de la boucle une fois le livre trouvé et modifié
+        }
+    }
+    return $biblio;
+}
--- HTML.CSS.JS.SQL/PHP/PHP/FORMULAIRE/Soutien/PHP/Exercice maison 130624/index.php
+++ HTML.CSS.JS.SQL/PHP/PHP/FORMULAIRE/Soutien/PHP/Exercice maison 130624/index.php
@@ -2,8 +2,73 @@
 
 require "fonction.php";
 
+while(true){
+echo "Menu Bibliothèque";
+echo "\n";
+echo "\n";
+echo "1.Voir le tableau";
+echo "\n";
+echo "2.Ajouter un livre";
+echo "\n";
+echo "3.Modifier un livre";
+echo "\n";
+echo "4.Afficher les livres";
+echo "\n";
+echo "5.Quitter le menu ?";
+echo "\n";
+$Selection = readline('Sélectionner un numéro correspondant à une option du tableau:  ');
 
-$ID = readline('Veuillez sélectionner un ID');
+switch($Selection){
+     case 1: 
+        echo "Vous avez choisi l'option 1: Voir le tableau";
+        echo "\n";
+        print_r($biblio);
+        break;
+     case 2: 
+        echo "Vous avez choisi l'option 2: Ajouter un livre";
+        echo "\n";
+        echo "\n";
+        $Titre = readline('Veuillez entrer un titre ');
+        echo "\n";
+        $Auteur = readline('Veuillez entrer un Auteur ');
+        echo "\n";
+        $Edition = readline('Veuillez entrer une Edition ');
+        echo "\n";
+        $Annee = readline('Veuillez entrer une Année ');
+        
+        Ajouter($biblio, $Auteur, $Edition, $Titre, $Annee);
+        print_r($biblio);
+        break;
+    case 3:
+        echo "Vous avez choisi l'option 3: Modifier";
+        echo "\n";
+        echo "\n";
+        $ChoixID = readline("Sélectionner l'ID: ");
+        $ModifAuteur = readline("Entrez le nouvel auteur: ");
+        $ModifEdition = readline("Entrez le nouvel éditeur: ");
+        
+        $biblio = modifierLivre($biblio, $ChoixID, $ModifAuteur, $ModifEdition);
+        
+        // // Afficher le résultat pour vérifier les modifications
+        print_r($biblio);
+        break;
+    case 4:
+        echo "Vous avez choisi l'option 4: Afficher les livres de la bibliothèque";
+        echo "\n";
+        echo "\n";
+        AfficherLivre($biblio);
+
+
+
+}
+
+
+
+}
+
+
+
+// $ID = readline('Veuillez sélectionner un ID');
 // echo print_r(AfficherLivre($Livres, $ID, $Livre1, $Livre2, $Livre3, $Livre4));
 
 
@@ -20,5 +85,9 @@ $ID = readline('Veuillez sélectionner un ID');
 // $ModifAuteur = readline("Sélectionner l'auteur du livre que vous souhaitez modifier");
 // $ModifEdition = readline("Sélectionner l'Edition du livre que vous souhaitez modifier");
 
-// modifierLivre($Livres, $ModifAuteur, $ModifEdition, $ChoixID);
+// $biblio = modifierLivre($biblio, $ChoixID, $ModifAuteur, $ModifEdition);
 
+// // Afficher le résultat pour vérifier les modifications
+// print_r($biblio);
+
+// echo AfficherLivre($ID, $Livre1, $Livre2, $Livre3, $Livre4);
